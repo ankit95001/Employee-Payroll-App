6a6b8596403065d582e16d463d1731a2de68962e
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-24 13:41:15 +0530

    [Ankit] Controllers are added

--- src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeeController.java
@@ -1,14 +1,54 @@
 package com.bridgelabz.employeepayrollapp.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.employeepayrollapp.entities.Employee;
+import com.bridgelabz.employeepayrollapp.services.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+    final EmployeeService employeeService;
+    final Employee employee;
+
+    EmployeeController(EmployeeService employeeService,Employee employee){
+        this.employeeService=employeeService;
+        this.employee=employee;
+    }
+
     @GetMapping
     public String welcomeEmployee(){
         return "Welcome to Employee Payroll App!";
+    }
+
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployee(){
+        return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeByID(@PathVariable Long id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String name, @RequestParam String department, @RequestParam Double salary ){
+        employee.setName(name);
+        employee.setId(null);
+        employee.setSalary(salary);
+        return employeeService.saveEmployee(employee);
+    }
+
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return "Employee deleted successfully";
     }
 }
