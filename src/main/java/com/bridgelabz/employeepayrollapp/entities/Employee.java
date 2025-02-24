5351b9bff27d34ecb3b7c0d4d657323b24240678
Ankit_kumar <ankit95001kumar@gmail.com>
2025-02-24 13:24:09 +0530

    [Ankit] Employee Entity Created

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/entities/Employee.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employeepayrollapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double salary;
+}
